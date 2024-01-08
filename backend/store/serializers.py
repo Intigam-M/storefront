b3c39da6403b591cb331c3aeb5083340e317c19f
Intigam Mammadli <intigam.mammadli@gmail.com>
2024-01-08 21:08:29 +0400

    Add images field to ProductSerializer and prefetch related images in ProductViewSet

--- backend/store/serializers.py
+++ backend/store/serializers.py
@@ -25,15 +25,19 @@ class ProductImageSerializer(serializers.ModelSerializer):
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    images = ProductImageSerializer(many=True, read_only=True)
+
     class Meta:
         model = Product
-        fields = ['id', 'title', 'description', 'slug', 'inventory', 'unit_price', 'price_with_tax', 'collection']
+        fields = ['id', 'title', 'description', 'slug', 'inventory',
+                  'unit_price', 'price_with_tax', 'collection', 'images']
 
     price_with_tax = serializers.SerializerMethodField(
         method_name='calculate_tax')
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
+
 
    
 class ReviewSerializer(serializers.ModelSerializer):
